840f770158bee40d7bf32f9c24b27821c933e158
fady17 <fady17@github.com>
2024-10-19 04:54:33 +0300

    ready for production i hope

--- src/components/textDemo.tsx
+++ src/components/textDemo.tsx
@@ -1,13 +1,13 @@
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const words = `
-Welcome to Fady's Agency! 
+Welcome to the Agency! 
 
-Driven by passion and creativity, this agency is dedicated to turning your ideas into reality. 
-With a focus on personalized solutions, Fady combines technical expertise with a unique perspective to deliver impactful results. 
-Whether you need a full-stack web application, custom AI chatbots, or data analysis tools, every project is approached with attention to detail and a commitment to excellence. 
+this agency is dedicated to turning your ideas into reality. 
+With a focus on personalized solutions, We combines technical expertise with a unique perspective to deliver impactful results. 
+Whether you need a full-stack web application, custom AI chatbots, 
 
-Let’s embark on this journey together and bring your vision to life!
+
 `;
 
 export function TextGenerateEffectDemo() {
